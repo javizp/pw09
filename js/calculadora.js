3e5e790bbc1d3f4bee18d1b319b6a3f43cdb81fa
javizp <javierci_97@hotmail.com>
2018-11-01 10:03:40 -0700

    Avance Calculadora

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,7 +1,51 @@
+window.onload = function() {
+	var operador = "";
+	
+	var colorAmarillo = function(){
+		this.style.background = "yellow";
+	}
+	var colorBlanco = function(){
+		this.style.background = "white";
+	}
 
-var colorAmarillo = function(x){
-	x.style.background = "yellow";
-}
-var colorBlanco = function(x){
-	x.style.background = "white";
+	var operando1 = document.getElementById("operando1");
+	operando1.addEventListener("focus", colorAmarillo);
+	operando1.addEventListener("focusout", colorBlanco);
+
+	var numeros = function(){
+		var valor = this.value;
+		if (operador=="") { //Operando1
+			var valorInput = document.getElement("operando1").value;
+			if (valorInput=="0") {
+				document.getElementById("operando1").value = "";	
+			}
+			document.getElementById("operando1").value+=valor;
+		} else { //Operando2
+			var valorInput = document.getElement("operando2").value;
+			if (valorInput=="0") {
+				document.getElementById("operando2").value = "";
+			}
+			document.getElementById("operando2").value+=valor;
+		}
+	}
+	var cero = document.getElementById("cero");
+	cero.addEventListener("click",numeros);
+	var uno = document.getElementById("uno");
+	uno.addEventListener("click",numeros);
+	var dos = document.getElementById("dos");
+	dos.addEventListener("click",numeros);
+	var tres = document.getElementById("tres");
+	tres.addEventListener("click",numeros);
+	var cuatro = document.getElementById("cuatro");
+	cuatro.addEventListener("click",numeros);
+	var cinco = document.getElementById("cinco");
+	cinco.addEventListener("click",numeros);
+	var seis = document.getElementById("seis");
+	seis.addEventListener("click",numeros);
+	var siete = document.getElementById("siete");
+	siete.addEventListener("click",numeros);
+	var ocho = document.getElementById("ocho");
+	ocho.addEventListener("click",numeros);
+	var nueve = document.getElementById("nueve");
+	nueve.addEventListener("click",numeros);
 }
